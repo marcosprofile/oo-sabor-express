3c6d2c63cbcba1040e86d4c935382f631530983c
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-19 01:42:16 -0300

    chore: constructor

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,15 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("Pizza", "Italiana")
 
-restaurante_pizza = Restaurante()
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
+# print(dir(restaurante_praca))
