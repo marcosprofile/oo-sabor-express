25f632e54f87853ac557100a12922fb4c5e2c528
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-19 02:10:48 -0300

    chore: more about properties

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,24 +2,24 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
 
     def listar_restaurantes():
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '✅ Ativo' if self._ativo else '⛔ Inativo'
 
-restaurante_praca = Restaurante("Praça", "Gourmet")
-restaurante_pizza = Restaurante("Pizza", "Italiana")
+restaurante_praca = Restaurante("praça", "gourmet")
+restaurante_pizza = Restaurante("pizza", "italiana")
 
 print(f'{'Nome do restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Status'}')
 Restaurante.listar_restaurantes()
