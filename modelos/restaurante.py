c9b53a2ca9042bc0a66bb023e9ce694345010cda
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-19 01:54:52 -0300

    chore: methods

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,15 +1,20 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 restaurante_praca = Restaurante("Praça", "Gourmet")
 restaurante_pizza = Restaurante("Pizza", "Italiana")
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
-
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-# print(dir(restaurante_praca))
+Restaurante.listar_restaurantes()
