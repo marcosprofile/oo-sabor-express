8e764beb28fcb18e3bb3d7b04cce3faa842b4d61
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-19 02:54:21 -0300

    refactor: validations in assessments

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from modelos.restaurante import Restaurante
 
 restaurante_mexicano = Restaurante('mexicano', 'mexican food')
 restaurante_mexicano.receber_avaliacao('John', 10)
-restaurante_mexicano.receber_avaliacao('Doe', 8)
-restaurante_mexicano.receber_avaliacao('James', 7)
+restaurante_mexicano.receber_avaliacao('Doe', 5)
+restaurante_mexicano.receber_avaliacao('James', 2)
 
 def main():
     Restaurante.listar_restaurantes()
